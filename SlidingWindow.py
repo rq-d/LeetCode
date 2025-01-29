63e21e8f75935ae71af9f2e415350d1e517fb9b2
Raul Quintana <raulybills@gmail.com>
2025-01-29 16:59:22 -0500

    Sliding Window problem

---
+++ SlidingWindow.py
@@ -0,0 +1,21 @@
+
+        
+s = "pwwkew"
+
+# sliding window
+charSet = set()
+l = 0
+r=0
+res = 0
+
+for r in range(len(s)):
+
+  while s[r] in charSet: # we found a repeated character, so remove the left most character from the substring
+    charSet.remove(s[l])
+    l+=1  # shift the window
+
+
+  charSet.add(s[r]) # now add the new character
+  res = max(res, r-l+1) # count the number of characters in the substring and compare to previous result
+
+print (res)
